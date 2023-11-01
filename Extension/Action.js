72a92de0d062a07c8520fb6e49eb189c326759a9
Yulian Gyurov <julian.gyuroff@gmail.com>
2023-11-01 19:44:01 +0200

    Challenge1: Add a bar button item that lets users select from a handful of prewritten example scripts, shown using a UIAlertController – at the very least your list should include the example we used in this project.

--- Extension/Action.js
+++ Extension/Action.js
@@ -3,7 +3,13 @@ var Action = function () {}
 Action.prototype = {
     
 run: function(parameters) {
-    parameters.completionFunction({"URL": document.URL, "title": document.title})
+    parameters.completionFunction({
+        "URL": document.URL,
+        "title": document.title,
+        "body": document.body.innerText,
+        "head": document.head.innerText,
+        "doctype": document.doctype
+    })
 },
 finalize: function(parameters) {
     var customJavaScript = parameters["customJavaScript"];
--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -14,11 +14,16 @@ class ActionViewController: UIViewController {
 
     var pageTitle = ""
     var pageURL = ""
+    var pageBody = ""
+    var pageHead = ""
+    var pageDoctype = ""
     
      override func viewDidLoad() {
         super.viewDidLoad()
          
-         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+         let select = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(selectScript))
+         let done = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+         navigationItem.rightBarButtonItems = [done,select]
          
          let notificationCenter = NotificationCenter.default
          notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -34,6 +39,9 @@ class ActionViewController: UIViewController {
                     //print(javaScriptValues)
                     self?.pageTitle = javaScriptValues["title"] as? String ?? ""
                     self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    self?.pageBody = javaScriptValues["body"] as? String ?? ""
+                    self?.pageHead = javaScriptValues["head"] as? String ?? ""
+                    self?.pageDoctype = javaScriptValues["doctype"] as? String ?? ""
                     
                     DispatchQueue.main.async {
                         self?.title = self?.pageTitle
@@ -69,7 +77,27 @@ class ActionViewController: UIViewController {
         script.scrollIndicatorInsets = script.contentInset
         let selectedRange = script.selectedRange
         script.scrollRangeToVisible(selectedRange)
-                
+    }
+    
+    @objc func selectScript() {
+        let ac = UIAlertController(title: "Select script", message: "from prepared javascripts", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "alert(document.title)", style: .default, handler: javascriptSelector))
+        ac.addAction(UIAlertAction(title: "alert(document.URL)", style: .default, handler: javascriptSelector))
+        ac.addAction(UIAlertAction(title: "alert(document.cookie)", style: .default, handler: javascriptSelector))
+        ac.addAction(UIAlertAction(title: "alert(document.body.innerText)", style: .default, handler: javascriptSelector))
+        ac.addAction(UIAlertAction(title: "alert(document.compatMode)", style: .default, handler: javascriptSelector))
+        ac.addAction(UIAlertAction(title: "alert(document.contentType)", style: .default, handler: javascriptSelector))
+        ac.addAction(UIAlertAction(title: "alert(document.location)", style: .default, handler: javascriptSelector))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func javascriptSelector(alert: UIAlertAction) {
+        
+        script.text = ""
+        script.text = alert.title
+        
+        
     }
 
 }
